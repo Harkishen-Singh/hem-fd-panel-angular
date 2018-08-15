7efd006f5028cb22012a65c6ceda9501f1186d5f
Harkishen-Singh <harkishensingh@hotmail.com>
2018-08-15 22:53:09 +0530

    added volunteers support

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.config(function($routeProvider,$locationProvider){
         controller:'patientsController',
         title:'View More - Patients'  
     })
+    .when('/volunteers', {
+        templateUrl:'./html_components/volunteer.html',
+        controller:'volunteerController',
+        title:'Volunteers'
+    })
+    .when('/volunteers/view', {
+        templateUrl:'./html_components/volunteerView.html',
+        controller:'volunteerController',
+        title:'View More - Volunteers'  
+    })
 })
 app.controller('defaultController', function($location){
     $location.path('/login');
@@ -106,4 +116,55 @@ app.controller('patientsController', function($scope,$location,$http) {
         })
     }
     
+})
+app.controller('volunteerController', function($http,$scope,$location) {
+    $scope.volunteersinit = function() {
+        console.warn('volunteerinit called')
+        $http({
+            url:global.url + '/getVolunteers',
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+            })
+            .then(res=>{
+                res = res.data;
+                if(res['Success']=='Y'){
+                    console.warn(res['value'][0]);
+                    $scope.vol = res.value;
+                }
+                    
+                else
+                    console.error('some err while receiving patients info from server')
+            })
+    }
+    $scope.volunteerViewMore = function() {
+        console.warn('volunteerViewMore called')
+        $http({
+            url:global.url + '/volunteersViewMore',
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            data:'adhaar='+$location.search().adhaar
+        })
+        .then(res => {
+            res = res.data;
+            console.warn(res['value']);
+            if(res.Success == 'Y'){
+                $scope.timeclient = res['value']['timeclient']
+                $scope.day = res['value']['dayclient']
+                $scope.gender = res['value']['gender']
+                $scope.fname = res['value']['firstname']
+                $scope.lname = res['value']['lastname']
+                $scope.mobile = res['value']['mobile']
+                $scope.dob = res['value']['dob']
+                $scope.address = res['value']['address']
+                $scope.aadhaar = res['value']['aadhaar']
+                $scope.edu = res['value']['educationalqualifications']
+            }
+            else
+                console.error('some err while patients view more info from server')
+        })
+    }
 })
