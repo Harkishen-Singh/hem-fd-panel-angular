3ef7cfd992007de86aec6c8983f17b4e5e24e9d0
Harkishen-Singh <harkishensingh@hotmail.com>
2018-08-17 22:18:48 +0530

    this

---
+++ angular_serve.js
@@ -0,0 +1,15 @@
+const express = require('express'),app=express(),
+    port = process.env.PORT || 8000
+;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    console.warn('got home request')
+    res.sendFile(__dirname+'/templates/index.html')
+})
+
+app.listen(port,'0.0.0.0' , (e)=>{
+    if(e) throw e;
+    console.warn('running at 8000')
+})
