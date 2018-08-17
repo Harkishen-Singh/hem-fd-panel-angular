1266e51521f13947fbb935da457c31f0219419f0
Harkishen-Singh <harkishensingh@hotmail.com>
2018-08-17 23:24:34 +0530

    this

--- angular_serve.js
+++ angular_serve.js
@@ -2,12 +2,12 @@ const express = require('express'),app=express(),
     port = process.env.PORT || 8000
 ;
 
-app.use(express.static('public'))
+app.use(express.static(__dirname+'/app'))
 
-app.get('/', (req, res) => {
-    console.warn('got home request')
-    res.sendFile(__dirname+'/templates/index.html')
-})
+// app.get('/', (req, res) => {
+//     console.warn('got home request')
+//     res.sendFile(__dirname+'/templates/index.html')
+// })
 
 app.listen(port,'0.0.0.0' , (e)=>{
     if(e) throw e;
